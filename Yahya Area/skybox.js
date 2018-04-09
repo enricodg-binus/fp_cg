8e986be8633713bb2cf69239c9868bc04f9ef672
〔≡◉︵◉≡〕 <anakaiti@users.noreply.github.com>
2018-04-10 00:04:39 +0700

    No bueno

---
+++ Yahya Area/skybox.js
@@ -0,0 +1,13 @@
+function Skybox(scene){
+    var skybox = BABYLON.MeshBuilder.CreateBox("skyBox", {size:1000.0}, scene);
+
+    var skyboxMaterial = new BABYLON.StandardMaterial("skyBox", scene);
+    skyboxMaterial.backFaceCulling = false;
+    skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture("res/texture/skybox/TropicalSunnyDay", scene);
+    skyboxMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
+    skyboxMaterial.diffuseColor = new BABYLON.Color3(0, 0, 0);
+    skyboxMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
+    skybox.material = skyboxMaterial;
+
+    return skybox;
+}
---
+++ Yahya Area/user.js
@@ -0,0 +1,37 @@
+const _skybox = Skybox;
+
+document.addEventListener("DOMContentLoaded", function(){
+    let canvas = document.getElementById("renderCanvas");
+    let engine = new BABYLON.Engine(canvas, true);
+    let scene = new BABYLON.Scene(engine);
+
+    gravity = new BABYLON.Vector3(0, -9.81, 0);
+    physicsEngine = new BABYLON.CannonJSPlugin();
+    scene.enablePhysics(gravity, physicsEngine);
+
+    // Create FreeCamera
+    camera = new BABYLON.FreeCamera("camera", new BABYLON.Vector3(0, 0, 10), scene); 
+    camera.setTarget(new BABYLON.Vector3(0, 4, -10));
+    camera.attachControl(canvas);
+
+    // Create light
+    let light = new BABYLON.HemisphericLight("hLight", new BABYLON.Vector3(0, 8, 0), scene);
+
+    // Create ground
+    let ground = BABYLON.Mesh.CreateGround('ground', 10, 10, 10, scene);
+
+    // Create skybox
+    let skybox = _skybox(scene);
+
+    // Load glTF
+    BABYLON.SceneLoader.Append('res/meshes/player/', 'player.gltf', scene);
+
+    engine.runRenderLoop(function(){
+        scene.render();
+    });
+
+    // the canvas/window resize event handler
+    window.addEventListener('resize', function(){
+        engine.resize();
+    });
+});
